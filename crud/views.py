4c1ffe87e5c9c33691a851380164351309781f67
poohbears123 <jmspinuela2020@gmail.com>
2025-05-31 21:54:30 +0800

    primary commit

---
+++ crud/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Item
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+
+def item_list(request):
+    items = Item.objects.all()
+    return render(request, 'crud/item_list.html', {'items': items})
+
+def item_detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return render(request, 'crud/item_detail.html', {'item': item})
+
+def item_create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        Item.objects.create(name=name, description=description)
+        return redirect('item_list')
+    return render(request, 'crud/item_form.html')
+
+def item_update(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == 'POST':
+        item.name = request.POST.get('name')
+        item.description = request.POST.get('description')
+        item.save()
+        return redirect('item_list')
+    return render(request, 'crud/item_form.html', {'item': item})
+
+def item_delete(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('item_list')
+    return render(request, 'crud/item_confirm_delete.html', {'item': item})
+
+def new_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username and password:
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+            return redirect('login')
+        else:
+            return render(request, 'crud/new_user.html', {'error': 'Please provide username and password'})
+    return render(request, 'crud/new_user.html')
+
+@login_required
+def delete_user(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    if request.method == 'POST':
+        user.delete()
+        return redirect('login')
+    return render(request, 'crud/delete_user_confirm.html', {'user': user})
---
+++ final_proj/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for final_proj project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from crud import views as crud_views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='crud/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('new_user/', crud_views.new_user, name='newuser'),
+    path('delete_user/<int:user_id>/', crud_views.delete_user, name='delete_user'),
+]
